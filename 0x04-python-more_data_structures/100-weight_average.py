d4367555700b778478546bef26c17fdcc52d733a
ASHISRAE <125803037+ASHISRAE@users.noreply.github.com>
2023-06-17 00:13:51 +0100

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    num = sum(y[0] * y[1] for y in my_list)
-    num2 = sum(y[1] for y in my_list)
-    return num / num2
+def square_matrix_map(matrix=[]):
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
