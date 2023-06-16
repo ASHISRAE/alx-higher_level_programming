5f9137cd3e1f25bd83f7e10d0b4d969fb4a4d147
ASHISRAE <125803037+ASHISRAE@users.noreply.github.com>
2023-06-17 00:12:04 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    sum = 0
-    index = 0
-    if roman_string is not None:
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                 'C': 100, 'D': 500, 'M': 1000}
-        listrom = list(roman.keys())
-        length = len(roman_string)
-        for char in roman_string:
-            # print("++ "+char)
-            if char in roman:
-                # print("+ "+char, roman[char])
-                if char in 'IXC':
-                    if index < length-1:
-                        if listrom[index+1] in 'VXLCDM':
-                            sum = sum + (roman[listrom[index+1]]-roman[char])
-                sum += roman[char]
-            index += 1
-    return sum
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_dictionary = {'I': 1, 'V': 5, 'X': 10,
+                        'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rst = 0
+    pre_v = 0
 
-
-"""
-check if current charachter index is equal to lenght
-"I" can be subtracted from "V" and "X",
-"X" can be subtracted from "L" and "C",
-"C" can be subtracted from "D" and "M".
-However, "V", "L", and "D" cannot be used to represent subtraction.
-"""
+    for chara in reversed(roman_string):
+        value = roman_dictionary.get(chara, 0)
+        if value >= pre_v:
+            rst += value
+        else:
+            rst -= value
+        pre_v = value
+    return rst
