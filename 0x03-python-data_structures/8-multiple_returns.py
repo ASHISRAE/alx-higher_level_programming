9d2a163a0f67e28beb09e230fadc9b97a9ddfa1e
ASHISRAE <125803037+ASHISRAE@users.noreply.github.com>
2023-06-30 18:04:57 +0100

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/8-multiple_returns.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    if sentence == "":
-        return (0, None)
-    len_s = len(sentence)
-    return (len_s, sentence[0])
--- 0x03-python-data_structures/README.md
+++
@@ -1,1 +0,0 @@
- how data types work in Python
